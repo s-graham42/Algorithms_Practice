b2076ba991e1d2777cf0d74c3d85bf70a81e4ebf
Stephen Graham <sgraham42@gmail.com>
2020-11-02 00:57:03 -0800

    fundamentals two complete

--- fundamentals_one/todo_two/todo_two.js
+++ fundamentals_one/todo_two/todo_two.js
@@ -77,3 +77,89 @@ var myOtherOtherArray = [false, true, false, false, true];
 console.log(greaterThanSecond(myArray).length);
 console.log(greaterThanSecond(myOtherArray).length);
 console.log(greaterThanSecond(myOtherOtherArray).length);
+
+// This Length, That Value
+// Given two numbers, return array of length num1 with each value num2. Print "Jinx!" if they are same.
+function lengthAndValue(num1, num2) {
+    newArr = []
+    for (var i = 0; i < num1; i++) {
+        if (i == num2) {
+            newArr[i] = "Jinx!";
+        }
+        else {
+            newArr[i] = num2;
+        }
+    }
+    return newArr;
+}
+
+console.log(lengthAndValue(6, 3))
+console.log(lengthAndValue(10, 11))
+console.log(lengthAndValue(12, 7))
+
+// Fit the First Value
+// Your function should accept an array.
+// If value at [0] is greater than array’s length, print "Too big!";
+// if value at [0] is less than array’s length, print "Too small!";
+// otherwise print "Just right!".
+
+function fitFirst(arr) {
+    if (arr[0] > arr.length) {
+        message = "Too big!";
+    }
+    else if (arr[0] < arr.length) {
+        message = "Too small!";
+    }
+    else {
+        message = "Just right!";
+    }
+    return message;
+}
+
+var myArray = [11, 21, 31, 41, 51, 61, 71, 81, 91];
+var myOtherArray = [1,3,5,7,9,13];
+var myOtherOtherArray = [false, true, false, false, true];
+
+console.log(fitFirst(myArray));
+console.log(fitFirst(myOtherArray));
+console.log(fitFirst(myOtherOtherArray));
+
+// Fahrenheit to Celsius
+// Kelvin wants to convert between temperature scales. 
+// Create fahrenheitToCelsius(fDegrees) that accepts a number of degrees in Fahrenheit
+// and returns the equivalent temperature as expressed in Celsius degrees.
+// For review, Fahrenheit = (9/5 * Celsius) + 32.
+
+function fahrenheitToCelsius(degf) {
+    return (((degf - 32) * 5) / 9);
+}
+
+console.log(fahrenheitToCelsius(32));
+console.log(fahrenheitToCelsius(212));
+console.log(fahrenheitToCelsius(72));
+
+// Celsius to Fahrenheit
+// Create celsiusToFahrenheit(cDegrees) that accepts number of degrees Celsius, 
+// and returns the equivalent temperature expressed in Fahrenheit degrees.
+
+function celsiusToFahrenheit(cDegrees) {
+    return  ((9/5 * cDegrees) + 32);
+}
+
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(100));
+console.log(celsiusToFahrenheit(22));
+
+// (Optional): Do Fahrenheit and Celsius values equate at a certain number?
+// The scientific calculation can be complex, so for this challenge just try a series of Celsius integer values starting at 200,
+// going downward (descending), checking whether it is equal to the corresponding Fahrenheit value.
+
+function findEquality(start) {
+    for (var i = start; i > -200; i--) {
+        if (i == celsiusToFahrenheit(i)) {
+            console.log("Yes!! Equal at: " + i);
+        }
+    }
+}
+
+findEquality(200);
