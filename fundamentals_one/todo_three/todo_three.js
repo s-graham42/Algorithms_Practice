e2c5c8800bbbc71ef70e178368e909b73775aa32
Stephen Graham <sgraham42@gmail.com>
2021-02-23 20:02:09 -0800

    js fundamentals todo 3 complete

---
+++ fundamentals_one/todo_three/todo_three.js
@@ -0,0 +1,230 @@
+class Solution {
+    constructor() {}
+    // Biggie Size
+    // Given an array, write a function that changes all positive numbers in the array to “big”.
+    // Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1,"big","big",-5].
+    biggieSize(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                arr[i] = "Big";
+            }
+        }
+        return arr;
+    }
+    // Print Low, Return High
+    // Create a function that takes an array of numbers. The  function should print the lowest value in the array,
+    // and return the highest value in the array.
+    printLowReturnHigh(arr) {
+        var lowest;
+        var highest;
+        if (arr.length > 0){
+            lowest = arr[0];
+            highest = arr[0];
+            for (var i = 1; i < arr.length; i++) {
+                if (arr[i] < lowest) {
+                    lowest = arr[i];
+                } else if (arr[i] > highest) {
+                    highest = arr[i];
+                }
+            }
+            console.log("Lowest: " + lowest);
+            return highest;
+        }
+    }
+
+    // Print One, Return Another
+    // Build a function that takes an array of numbers. The function should print the second-to-last value in the array,
+    // and return first odd value in the array.
+    printOneRetOther(arr) {
+        if (arr.length > 1) {
+            console.log("Second to last value: " + arr[arr.length - 2]);
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 0) {
+                    return arr[i];
+                }
+            }
+        }
+    }
+
+    // Double Vision
+    // Given an array, create a function to return a new array where each value in the original has been doubled.
+    // Calling double([1,2,3]) should return [2,4,6] without changing original.
+    doubleArray(arr) {
+        var newArray = [];
+        for (var i = 0; i < arr.length; i++) {
+            newArray.push(arr[i] * 2);
+        }
+        return newArray;
+    }
+
+    // Count Positives
+    // Given an array of numbers, create a function to replace last value with the number of positive values.
+    // Example,  countPositives([-1,1,1,1]) changes array to [-1,1,1,3] and returns it.
+    countPositives(arr) {
+        var sum = 0;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                sum += 1;
+            }
+        }
+        if (sum > 0) {
+            arr[arr.length -1] = sum;
+        }
+        return arr;
+    }
+
+    // Evens and Odds
+    // Create a function that accepts an array. Every time that array has three odd values in a row, print
+    // "That’s odd!" Every time the array has three evens in a row, print "Even more so!"
+    evenAndOdds(arr) {
+        var odds = 0;
+        var evens = 0;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                evens += 1;
+                odds = 0;
+            } else {
+                odds += 1;
+                evens = 0;
+            }
+
+            if (odds == 3) {
+                console.log("That's odd!");
+                odds = 0;
+            } else if (evens == 3) {
+                console.log("Even more so!")
+                evens = 0;
+            }
+        }
+    }
+
+    // Increment the Seconds
+    // Given arr, add 1 to odd elements ([1], [3], etc.), console.log all values and return arr.
+    incrementSeconds(arr) {
+        console.log(JSON.stringify(arr));
+        for (var i = 1; i < arr.length; i += 2) {
+            arr[i] = arr[i] + 1;
+        }
+        console.log(JSON.stringify(arr));
+        return arr;
+    }
+
+    // Previous Lengths
+    // You are passed an array containing strings. Working within that same array, replace each string with a number
+    // – the length of the string at previous array index – and return the array.
+    previousLengths(stringArr) {
+        for (var i = stringArr.length - 1; i > 0; i--) {
+            stringArr[i] = stringArr[i-1].length;
+        }
+        stringArr[0] = 0;
+        return stringArr;
+    }
+
+    // Add Seven to Most
+    // Build a function that accepts an array. Return a new array with all values except first, adding 7 to each.
+    // Do not alter the original array.
+    addSevenToMost(arr) {
+        var newArr = [];
+        for (var i = 1; i < arr.length; i++) {
+            newArr.push(arr[i] + 7);
+        }
+        return newArr;
+    }
+
+    // Reverse Array
+    // Given array, write a function to reverse values, in-place.
+    // Example: reverse([3,1,6,4,2]) returns same array, containing [2,4,6,1,3].
+    reverseArray(arr) {
+        for (var i = 0; i < arr.length / 2; i ++) {
+            [arr[i], arr[arr.length - (i + 1)]] = [arr[arr.length - (i + 1)], arr[i]];
+        }
+        return arr;
+    }
+
+    // Outlook: Negative
+    // Given an array, create and return a new one containing all the values of the provided array,
+    // made negative (not simply multiplied by -1). Given [1,-3,5], return [-1,-3,-5].
+    negativeArray(arr) {
+        for (var i = 0; i < arr.length; i ++) {
+            if (arr[i] > 0) {
+                arr[i] = arr[i] *= -1;
+            }
+        }
+        return arr;
+    }
+
+    // Always Hungry
+    // Create a function that accepts an array, and prints "yummy" each time one of the values is equal to "food". If no array elements are
+    // "food", then print "I'm hungry" once.
+    hungry(stringArr) {
+        var foods = false;
+        for (var i = 0; i < stringArr.length; i ++) {
+            if (stringArr[i] == "food") {
+                console.log("Yummy!");
+                foods = true;
+            }
+        }
+        if (foods == false) {
+            console.log("I'm hungry...");
+        }
+    }
+
+    // Swap Toward the Center
+    // Given array, swap first and last, third and third-tolast, etc. Input[true,42,"Ada",2,"pizza"] becomes ["pizza",42,"Ada",2,true]. 
+    // Change [1,2,3,4,5,6] to [6,2,4,3,5,1].
+    swapToCenter(arr) {
+        for (var i = 0; i < arr.length / 2; i += 2) {
+            [arr[i], arr[arr.length - (i + 1)]] = [arr[arr.length - (i + 1)], arr[i]];
+        }
+        return arr;
+    }
+
+    // Scale the Array
+    // Given array arr and number num, multiply each arr value by num, and return the changed arr.
+    scaleArray(arr, num) {
+        for (var i = 0; i < arr.length; i ++) {
+            arr[i] = arr[i] * num;
+        }
+        return arr;
+    }
+
+}
+
+var sol = new Solution();
+var myArray = [-1, 2, 6, 4, 5, -6, 7, 9, 11];
+var myOtherArray = [0, 0, 0, 0, 0, 0]
+var myEmptyArray = [];
+var myStringArray = ["Ia", "Bob", "Chip", "Steve", "Benji", "Ulrich", "Normann"];
+// console.log(sol.biggieSize(myArray));
+
+// console.log("Highest: " + sol.printLowReturnHigh(myArray));
+
+// console.log("First odd value: " + sol.printOneRetOther(myArray));
+// console.log("First odd value: " + sol.printOneRetOther(myEmptyArray));
+
+// console.log("myArray Doubled: " + sol.doubleArray(myArray));
+
+// console.log("Count Positives: " + sol.countPositives(myArray));
+// console.log("Count Positives: " + sol.countPositives(myOtherArray));
+
+// sol.evenAndOdds(myArray);
+// sol.evenAndOdds(myOtherArray);
+
+// console.log(JSON.stringify(sol.incrementSeconds(myArray)));
+
+// console.log(JSON.stringify(sol.previousLengths(myStringArray)));
+
+// console.log(JSON.stringify(sol.addSevenToMost(myArray)));
+
+// console.log(JSON.stringify(sol.reverseArray(myArray)));
+
+// console.log(JSON.stringify(sol.negativeArray(myArray)));
+
+// sol.hungry(myStringArray);
+
+// console.log(JSON.stringify(sol.swapToCenter(myStringArray)));
+// console.log(JSON.stringify(sol.swapToCenter(myArray)));
+
+console.log(JSON.stringify(sol.scaleArray(myArray, 10)));
+
+
--- fundamentals_one/todo_two/todo_two.py
+++ fundamentals_one/todo_two/todo_two.py
@@ -32,6 +32,61 @@ class Solution:
                 print(i)
         return numGreater
 
+    # Values Greater than Second, Generalized
+    # Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value.
+    # Print how many values this is. What will you do if the array is only one element long?
+    def gtSecondGeneralized(self, arr):
+        if len(arr) < 2:
+            return "Error: Array Must Be longer than 1 item."
+        newArr = []
+        numGreater = 0
+        for i in arr:
+            if i > arr[1]:
+                numGreater += 1
+                newArr.append(i)
+        print(f"Values Greater than second index: {numGreater}")
+        return newArr
+
+    # This Length, That Value
+    # Given two numbers, return array of length num1 with each value num2. Print "Jinx!" if they are same.
+    def lengthAndValue(self, num1, num2):
+        newArr = []
+        for i in range(num1):
+            newArr.append(num2)
+        if num1 == num2:
+            print("--  Jinx!  --")
+        return newArr
+
+    # Fit the First Value
+    # Your function should accept an array. If value at [0] is greater than array’s length, print "Too big!";
+    # if value at [0] is less than array’s length, print "Too small!"; otherwise print "Just right!".
+    def fitFirstValue(self, arr):
+        if arr[0] > len(arr):
+            print("Too Big!")
+        elif arr[0] < len(arr):
+            print("Too Small!")
+        else:
+            print("Just Right.")
+
+    # Fahrenheit to Celsius
+    # Kelvin wants to convert between temperature scales. Create fahrenheitToCelsius(fDegrees) that accepts a
+    # number of degrees in Fahrenheit and returns the equivalent temperature as expressed in Celsius degrees.
+    # For review, Fahrenheit = (9/5 * Celsius) + 32.
+    def fahrenheitToCelsius(self, fDegrees):
+        return round((fDegrees-32) * (5/9), 2)
+
+    # Celsius to Fahrenheit
+    # Create celsiusToFahrenheit(cDegrees) that accepts number of degrees Celsius, and returns the equivalent temperature expressed in Fahrenheit degrees.
+    def celsiusToFahrenheit(self, cDegrees):
+        return round(((9/5) * cDegrees) + 32, 2)
+
+    # (Optional): Do Fahrenheit and Celsius values equate at a certain number? The scientific calculation can be complex,
+    # so for this challenge just try a series of Celsius integer values starting at 200, going downward (descending),
+    # checking whether it is equal to the corresponding Fahrenheit value.
+    def findEquivalentFdegrees(self):
+        for i in range(200, -360, -1):
+            if self.celsiusToFahrenheit(i) == i:
+                print(f"Equivalent Degree Value found: {i} degrees in Celcius is {i} degrees in Fahrenheit.")
 
 sol = Solution()
 # print(f"CountDown: {sol.countDown(5)}")
@@ -42,5 +97,19 @@ sol = Solution()
 # # myArr = ['what?', 4, 6, 8, 0]
 # # print(f"firstPlusLength: {sol.firstPlusLength(myArr)}")
 # print(f"firstPlusLength: {sol.firstPlusLength([False, 4, 6, 8, 0])}")
-myArr = [16, 11, 18, 3, 3, 10, 2, 17]
-# print(f"Values Greater than second index: {sol.gtSecond(myArr)}")
+# myArr = [16, 11, 18, 3, 3, 10, 2, 17]
+# myOtherArray = [3,2,3]
+# # print(f"Values Greater than second index: {sol.gtSecond(myArr)}")
+# print(sol.gtSecondGeneralized(myArr))
+# print(sol.gtSecondGeneralized(myOtherArray))
+
+# print(sol.lengthAndValue(12, "Howdy"))
+# print(sol.lengthAndValue(12, 12))
+# print(sol.lengthAndValue(1, True))
+
+# sol.fitFirstValue(myArr)
+
+print(sol.fahrenheitToCelsius(70))
+print(sol.celsiusToFahrenheit(21.11))
+
+sol.findEquivalentFdegrees()
